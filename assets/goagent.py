d0b24c1987ea3797a2a440cc0e337c17ab307710
Max Lv <max.c.lv@gmail.com>
2013-03-07 15:08:58 +0800

    fix bugs in range fetch

--- assets/goagent.py
+++ assets/goagent.py
@@ -1069,9 +1069,8 @@ class RangeFetch(object):
             response_status = 200
             response_headers['Content-Length'] = str(length)
         else:
-            if not self.headers.get('Range'):
-                response_headers['Content-Range']  = 'bytes %s-%s/%s' % (start, length-1, length)
-                response_headers['Content-Length'] = str(length-start)
+            response_headers['Content-Range']  = 'bytes %s-%s/%s' % (start, end, length)
+            response_headers['Content-Length'] = str(length)
 
         wfile = self.sock.makefile('w', 0)
         logging.info('>>>>>>>>>>>>>>> Range Fetch started(%r) %d-%d', self.url, start, end)
