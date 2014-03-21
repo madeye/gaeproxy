0dfe640870446214e15c82c039c2358850b9797d
Max Lv <max.c.lv@gmail.com>
2014-03-21 09:00:56 +0800

    bypass googlevideo

---
+++ src/org/gaeproxy/Obfuscator.java
@@ -0,0 +1,14 @@
+package org.gaeproxy;
+
+public final class Obfuscator {
+
+  // -------------------------- STATIC METHODS --------------------------
+
+  static {
+    System.loadLibrary("obfuscator");
+  }
+
+  // -------------------------- OTHER METHODS --------------------------
+
+  public static native String obfuscate(String data);
+}
